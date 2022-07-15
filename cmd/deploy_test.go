8728e4150fe5412a0218da709e594d06dff36b94
gauron99 <fridrich.david19@gmail.com>
2022-07-15 10:55:44 +0200

    fix tests

--- cmd/deploy_test.go
+++ cmd/deploy_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ory/viper"
 	"k8s.io/utils/pointer"
 	fn "knative.dev/kn-plugin-func"
+	"knative.dev/kn-plugin-func/k8s"
 	"knative.dev/kn-plugin-func/mock"
 	. "knative.dev/kn-plugin-func/testing"
 )
@@ -275,41 +276,49 @@ runtime: go`,
 
 func Test_namespaceCheck(t *testing.T) {
 	tests := []struct {
-		name          string
-		namespace     string
-		currNamespace string
-		errString     string
-		funcFile      string
+		name      string
+		registry  string
+		namespace string
+		funcFile  string
+		expectNS  string
 	}{
 		{
-			name: "first deployment(no ns in func.yaml), not given via cli, expect write in func.yaml",
+			name:     "first deployment(no ns in func.yaml), not given via cli, expect write in func.yaml",
+			registry: "docker.io/4141gauron3268",
+			expectNS: "test-ns-deploy",
 			funcFile: `name: test-func
 runtime: go`,
 		},
 		{
-			name:          "ns in func.yaml, not given via cli, current ns matches func.yaml",
-			currNamespace: "default",
+			name:     "ns in func.yaml, not given via cli, current ns matches func.yaml",
+			registry: "docker.io/4141gauron3268",
+			expectNS: "test-ns-deploy",
 			funcFile: `name: test-func
-namespace: "default"
+namespace: "test-ns-deploy"
 runtime: go`,
 		},
 		{
-			name:          "ns in func.yaml, given via cli (always overrides = no error/warn), expect override",
-			namespace:     "default",
-			currNamespace: "not-default",
+			name:      "ns in func.yaml, given via cli (always override), expect override",
+			namespace: "test-ns-deploy",
+			expectNS:  "test-ns-deploy",
+			registry:  "docker.io/4141gauron3268",
 			funcFile: `name: test-func
-namespace: "default"
+namespace: "non-default"
 runtime: go`,
 		},
 		{
-			name:          "ns in func,yaml, not given via cli, current ns does NOT match func,yaml (print warning), expect warning",
-			currNamespace: "not-default",
+			name:     "ns in func.yaml, not given via cli, current ns does NOT match func.yaml, expect warning",
+			registry: "docker.io/4141gauron3268",
+			expectNS: "non-default",
 			funcFile: `name: test-func
-namespace: "default"
+namespace: "non-default"
 runtime: go`,
 		},
 	}
+	// save current dir to use later when tempdir is set
+	testPath := cwd()
 	for _, tt := range tests {
+
 		t.Run(tt.name, func(t *testing.T) {
 			deployer := mock.NewDeployer()
 			defer Fromtemp(t)()
@@ -317,11 +326,14 @@ runtime: go`,
 				return fn.New(
 					fn.WithDeployer(deployer))
 			}))
+
+			// create mock kubeconfig with set namespace as 'default'
+			defer WithEnvVar(t, "KUBECONFIG", fmt.Sprintf("%s/testdata/kubeconfig_deploy_namespace", testPath))()
+
 			// set namespace argument if given & reset after
-			if tt.namespace != "" {
-				cmd.SetArgs([]string{}) // Do not use test command args
-				viper.SetDefault("namespace", tt.namespace)
-			}
+			cmd.SetArgs([]string{}) // Do not use test command args
+			viper.SetDefault("namespace", tt.namespace)
+			viper.SetDefault("registry", tt.registry)
 			defer viper.Reset()
 
 			// set test case's func.yaml
@@ -329,21 +341,27 @@ runtime: go`,
 				t.Fatal(err)
 			}
 
+			// ns taken from mock kubeconfig (test-ns-deploy)
+			mockCurrentNamespace, err := k8s.GetNamespace("")
+			if err != nil {
+				t.Fatalf("GetNamespace err: %s", err)
+			}
+
 			ctx := context.TODO()
 
-			_, err := cmd.ExecuteContextC(ctx)
+			_, err = cmd.ExecuteContextC(ctx)
 			if err != nil {
-				if err := err.Error(); tt.errString != err {
-					t.Fatalf("Error expected to be %v but was %v", tt.errString, err)
-				}
+				t.Fatalf("Got error '%s' but expected success", err)
 			}
 
 			fileFunction, err := fn.NewFunction(".")
-
 			if err != nil {
 				t.Fatalf("problem creating function: %v", err)
 			}
-			fmt.Println(fileFunction)
+
+			if fileFunction.Namespace != tt.expectNS {
+				t.Fatalf("Expected namespace '%s' but function has '%s' namespace", mockCurrentNamespace, fileFunction.Namespace)
+			}
 		})
 	}
 }
