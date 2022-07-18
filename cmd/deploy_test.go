73d1dc5de709537767e49160ca1be88d0ffc026c
gauron99 <fridrich.david19@gmail.com>
2022-07-18 15:25:33 +0200

    test2

--- cmd/deploy_test.go
+++ cmd/deploy_test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"testing"
 
@@ -116,6 +117,8 @@ created: 2009-11-10 23:00:00`,
 					fn.WithPipelinesProvider(pipeline),
 					fn.WithDeployer(deployer))
 			}))
+
+			defer WithEnvVar(t, "KUBECONFIG", fmt.Sprintf("%s/testdata/kubeconfig_deploy_namespace", cwd()))()
 			cmd.SetArgs([]string{}) // Do not use test command args
 
 			// TODO: the below viper.SetDefault calls appear to be altering
@@ -181,179 +184,178 @@ created: 2009-11-10 23:00:00`,
 	}
 }
 
-// func Test_imageWithDigest(t *testing.T) {
-// 	tests := []struct {
-// 		name      string
-// 		image     string
-// 		buildType string
-// 		pushBool  bool
-// 		funcFile  string
-// 		errString string
-// 	}{
-// 		{
-// 			name:      "valid full name with digest, expect success",
-// 			image:     "docker.io/4141gauron3268/static_test_digest:latest@sha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4e",
-// 			errString: "",
-// 			funcFile: `name: test-func
-// runtime: go`,
-// 		},
-// 		{
-// 			name:      "valid image name, build not 'disabled', expect error",
-// 			image:     "docker.io/4141gauron3268/static_test_digest:latest@sha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4e",
-// 			buildType: "local",
-// 			errString: "the --build flag 'local' is not valid when using --image with digest",
-// 			funcFile: `name: test-func
-// runtime: go`,
-// 		},
-// 		{
-// 			name:      "valid image name, --push specified, expect error",
-// 			image:     "docker.io/4141gauron3268/static_test_digest:latest@sha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4e",
-// 			pushBool:  true,
-// 			errString: "the --push flag 'true' is not valid when using --image with digest",
-// 			funcFile: `name: test-func
-// runtime: go`,
-// 		},
-// 		{
-// 			name:      "invalid digest prefix, expect error",
-// 			image:     "docker.io/4141gauron3268/static_test_digest:latest@Xsha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4e",
-// 			errString: "value 'docker.io/4141gauron3268/static_test_digest:latest@Xsha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4e' in --image has invalid prefix syntax for digest (should be 'sha256:')",
-// 			funcFile: `name: test-func
-// runtime: go`,
-// 		},
-// 		{
-// 			name:      "invalid sha hash length(added X at the end), expect error",
-// 			image:     "docker.io/4141gauron3268/static_test_digest:latest@sha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4eX",
-// 			errString: "sha256 hash in 'sha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4eX' from --image has the wrong length (65), should be 64",
-// 			funcFile: `name: test-func
-// runtime: go`,
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			deployer := mock.NewDeployer()
-// 			cmd := NewDeployCmd(NewClientFactory(func() *fn.Client {
-// 				return fn.New(
-// 					fn.WithDeployer(deployer))
-// 			}))
+func Test_imageWithDigest(t *testing.T) {
+	tests := []struct {
+		name      string
+		image     string
+		buildType string
+		pushBool  bool
+		funcFile  string
+		errString string
+	}{
+		{
+			name:      "valid full name with digest, expect success",
+			image:     "docker.io/4141gauron3268/static_test_digest:latest@sha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4e",
+			errString: "",
+			funcFile: `name: test-func
+runtime: go`,
+		},
+		{
+			name:      "valid image name, build not 'disabled', expect error",
+			image:     "docker.io/4141gauron3268/static_test_digest:latest@sha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4e",
+			buildType: "local",
+			errString: "the --build flag 'local' is not valid when using --image with digest",
+			funcFile: `name: test-func
+runtime: go`,
+		},
+		{
+			name:      "valid image name, --push specified, expect error",
+			image:     "docker.io/4141gauron3268/static_test_digest:latest@sha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4e",
+			pushBool:  true,
+			errString: "the --push flag 'true' is not valid when using --image with digest",
+			funcFile: `name: test-func
+runtime: go`,
+		},
+		{
+			name:      "invalid digest prefix, expect error",
+			image:     "docker.io/4141gauron3268/static_test_digest:latest@Xsha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4e",
+			errString: "value 'docker.io/4141gauron3268/static_test_digest:latest@Xsha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4e' in --image has invalid prefix syntax for digest (should be 'sha256:')",
+			funcFile: `name: test-func
+runtime: go`,
+		},
+		{
+			name:      "invalid sha hash length(added X at the end), expect error",
+			image:     "docker.io/4141gauron3268/static_test_digest:latest@sha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4eX",
+			errString: "sha256 hash in 'sha256:7d66645b0add6de7af77ef332ecd4728649a2f03b9a2716422a054805b595c4eX' from --image has the wrong length (65), should be 64",
+			funcFile: `name: test-func
+runtime: go`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			deployer := mock.NewDeployer()
+			cmd := NewDeployCmd(NewClientFactory(func() *fn.Client {
+				return fn.New(
+					fn.WithDeployer(deployer))
+			}))
 
-// 			// Set flags manually & reset after.
-// 			// Differs whether build was set via CLI (gives an error if not 'disabled')
-// 			// or not (prints just a warning)
-// 			if tt.buildType == "" {
-// 				cmd.SetArgs([]string{
-// 					fmt.Sprintf("--image=%s", tt.image),
-// 					fmt.Sprintf("--push=%t", tt.pushBool),
-// 				})
-// 			} else {
-// 				cmd.SetArgs([]string{
-// 					fmt.Sprintf("--image=%s", tt.image),
-// 					fmt.Sprintf("--build=%s", tt.buildType),
-// 					fmt.Sprintf("--push=%t", tt.pushBool),
-// 				})
-// 			}
-// 			defer cmd.ResetFlags()
+			// Set flags manually & reset after.
+			// Differs whether build was set via CLI (gives an error if not 'disabled')
+			// or not (prints just a warning)
+			if tt.buildType == "" {
+				cmd.SetArgs([]string{
+					fmt.Sprintf("--image=%s", tt.image),
+					fmt.Sprintf("--push=%t", tt.pushBool),
+				})
+			} else {
+				cmd.SetArgs([]string{
+					fmt.Sprintf("--image=%s", tt.image),
+					fmt.Sprintf("--build=%s", tt.buildType),
+					fmt.Sprintf("--push=%t", tt.pushBool),
+				})
+			}
+			defer cmd.ResetFlags()
 
-// 			// set test case's func.yaml
-// 			if err := os.WriteFile("func.yaml", []byte(tt.funcFile), os.ModePerm); err != nil {
-// 				t.Fatal(err)
-// 			}
+			// set test case's func.yaml
+			if err := os.WriteFile("func.yaml", []byte(tt.funcFile), os.ModePerm); err != nil {
+				t.Fatal(err)
+			}
 
-// 			ctx := context.TODO()
+			ctx := context.TODO()
 
-// 			_, err := cmd.ExecuteContextC(ctx)
-// 			if err != nil {
-// 				if err := err.Error(); tt.errString != err {
-// 					t.Fatalf("Error expected to be (%v) but was (%v)", tt.errString, err)
-// 				}
-// 			}
-// 		})
-// 	}
-// }
+			_, err := cmd.ExecuteContextC(ctx)
+			if err != nil {
+				if err := err.Error(); tt.errString != err {
+					t.Fatalf("Error expected to be (%v) but was (%v)", tt.errString, err)
+				}
+			}
+		})
+	}
+}
 
-// func Test_namespaceCheck(t *testing.T) {
-// 	tests := []struct {
-// 		name      string
-// 		registry  string
-// 		namespace string
-// 		funcFile  string
-// 		expectNS  string
-// 	}{
-// 		{
-// 			name:     "first deployment(no ns in func.yaml), not given via cli, expect write in func.yaml",
-// 			registry: "docker.io/4141gauron3268",
-// 			expectNS: "test-ns-deploy",
-// 			funcFile: `name: test-func
-// runtime: go`,
-// 		},
-// 		{
-// 			name:     "ns in func.yaml, not given via cli, current ns matches func.yaml",
-// 			registry: "docker.io/4141gauron3268",
-// 			expectNS: "test-ns-deploy",
-// 			funcFile: `name: test-func
-// namespace: "test-ns-deploy"
-// runtime: go`,
-// 		},
-// 		{
-// 			name:      "ns in func.yaml, given via cli (always override)",
-// 			namespace: "test-ns-deploy",
-// 			expectNS:  "test-ns-deploy",
-// 			registry:  "docker.io/4141gauron3268",
-// 			funcFile: `name: test-func
-// namespace: "non-default"
-// runtime: go`,
-// 		},
-// 		{
-// 			name:     "ns in func.yaml, not given via cli, current ns does NOT match func.yaml",
-// 			registry: "docker.io/4141gauron3268",
-// 			expectNS: "non-default",
-// 			funcFile: `name: test-func
-// namespace: "non-default"
-// runtime: go`,
-// 		},
-// 	}
+func Test_namespaceCheck(t *testing.T) {
+	tests := []struct {
+		name      string
+		registry  string
+		namespace string
+		funcFile  string
+		expectNS  string
+	}{
+		{
+			name:     "first deployment(no ns in func.yaml), not given via cli, expect write in func.yaml",
+			registry: "docker.io/4141gauron3268",
+			expectNS: "test-ns-deploy",
+			funcFile: `name: test-func
+runtime: go`,
+		},
+		{
+			name:     "ns in func.yaml, not given via cli, current ns matches func.yaml",
+			registry: "docker.io/4141gauron3268",
+			expectNS: "test-ns-deploy",
+			funcFile: `name: test-func
+namespace: "test-ns-deploy"
+runtime: go`,
+		},
+		{
+			name:      "ns in func.yaml, given via cli (always override)",
+			namespace: "test-ns-deploy",
+			expectNS:  "test-ns-deploy",
+			registry:  "docker.io/4141gauron3268",
+			funcFile: `name: test-func
+namespace: "non-default"
+runtime: go`,
+		},
+		{
+			name:     "ns in func.yaml, not given via cli, current ns does NOT match func.yaml",
+			registry: "docker.io/4141gauron3268",
+			expectNS: "non-default",
+			funcFile: `name: test-func
+namespace: "non-default"
+runtime: go`,
+		},
+	}
 
-// 	// save current dir to use later when tempdir is set
-// 	testPath := cwd()
-// 	for _, tt := range tests {
+	// save current dir to use later when tempdir is set
+	testPath := cwd()
+	for _, tt := range tests {
 
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			deployer := mock.NewDeployer()
-// 			defer Fromtemp(t)()
-// 			cmd := NewDeployCmd(NewClientFactory(func() *fn.Client {
-// 				return fn.New(
-// 					fn.WithDeployer(deployer))
-// 			}))
+		t.Run(tt.name, func(t *testing.T) {
+			deployer := mock.NewDeployer()
+			defer Fromtemp(t)()
+			cmd := NewDeployCmd(NewClientFactory(func() *fn.Client {
+				return fn.New(
+					fn.WithDeployer(deployer))
+			}))
 
-// 			// create mock kubeconfig with set namespace as 'default'
-// 			defer WithEnvVar(t, "KUBECONFIG", fmt.Sprintf("%s/testdata/kubeconfig_deploy_namespace", testPath))()
-// 			defer WithEnvVar(t, "KUBERNETES_MASTER", "/tmp/non-existent.config")()
+			// create mock kubeconfig with set namespace as 'default'
+			defer WithEnvVar(t, "KUBECONFIG", fmt.Sprintf("%s/testdata/kubeconfig_deploy_namespace", testPath))()
 
-// 			// set namespace argument if given & reset after
-// 			cmd.SetArgs([]string{}) // Do not use test command args
-// 			viper.SetDefault("namespace", tt.namespace)
-// 			viper.SetDefault("registry", tt.registry)
-// 			defer viper.Reset()
+			// set namespace argument if given & reset after
+			cmd.SetArgs([]string{}) // Do not use test command args
+			viper.SetDefault("namespace", tt.namespace)
+			viper.SetDefault("registry", tt.registry)
+			defer viper.Reset()
 
-// 			// set test case's func.yaml
-// 			if err := os.WriteFile("func.yaml", []byte(tt.funcFile), os.ModePerm); err != nil {
-// 				t.Fatal(err)
-// 			}
+			// set test case's func.yaml
+			if err := os.WriteFile("func.yaml", []byte(tt.funcFile), os.ModePerm); err != nil {
+				t.Fatal(err)
+			}
 
-// 			ctx := context.TODO()
+			ctx := context.TODO()
 
-// 			_, err := cmd.ExecuteContextC(ctx)
-// 			if err != nil {
-// 				t.Fatalf("Got error '%s' but expected success", err)
-// 			}
+			_, err := cmd.ExecuteContextC(ctx)
+			if err != nil {
+				t.Fatalf("Got error '%s' but expected success", err)
+			}
 
-// 			fileFunction, err := fn.NewFunction(".")
-// 			if err != nil {
-// 				t.Fatalf("problem creating function: %v", err)
-// 			}
+			fileFunction, err := fn.NewFunction(".")
+			if err != nil {
+				t.Fatalf("problem creating function: %v", err)
+			}
 
-// 			if fileFunction.Namespace != tt.expectNS {
-// 				t.Fatalf("Expected namespace '%s' but function has '%s' namespace", tt.expectNS, fileFunction.Namespace)
-// 			}
-// 		})
-// 	}
-// }
+			if fileFunction.Namespace != tt.expectNS {
+				t.Fatalf("Expected namespace '%s' but function has '%s' namespace", tt.expectNS, fileFunction.Namespace)
+			}
+		})
+	}
+}
